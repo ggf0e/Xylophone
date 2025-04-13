86fe9ae82d303312e919401af45087ca0c1c7442
ggf0e <ilegnosov@gmail.com>
2025-04-13 13:49:45 +0300

    add feedback

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,9 +2,11 @@ import UIKit
 import AVFoundation
 
 final class ViewController: UIViewController {
+    private let feedback = UIImpactFeedbackGenerator()
     private var player: AVAudioPlayer!
     
-    @IBAction private func KeyPressed(_ sender: UIButton) {
+    @IBAction private func keyPressed(_ sender: UIButton) {
+        vibrate(sender)
         playSouund(nameSound: sender.currentTitle!)
     }
     
@@ -13,7 +15,10 @@ final class ViewController: UIViewController {
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
+    private func vibrate(_ sender: UIButton) {
+        feedback.prepare()
+        feedback.impactOccurred(intensity: sender.frame.minX / 20)
+    }
     
-
 }
 
